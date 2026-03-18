e86be9e954ca0415bcf52d107cea75e0bbecfbac
asif7229islam-netizen <asif7229islam@gmail.com>
2026-03-18 15:10:07 +0600

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T09:10:07.058Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-18T09:10:10.922Z
+Commit: Fix broken links in documentation
